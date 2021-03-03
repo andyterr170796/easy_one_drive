b2846a80366e915ad7c6f3c8e9840480530cf1b0
andyterr170796 <70968080+andyterr170796@users.noreply.github.com>
2021-03-03 16:58:00 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,33 @@
+
+correo = '' #write your hotmail/outlook email
+contra = '' #write your password
+path = '' #write your chromedriver.exe path
+
+# Set the class to object "s"
+s = easy_one_drive(correo, contra, path)
+
+# First you must logging in
+s.logging_in()
+
+# Some examples here:
+## First for downloading files with patterns, you only must set URL where files are located, then the pattern (string, not list), and optionally the waiting time
+s.download_file('','_d') # Here all files that contains string '_d' in their names will be downloaded with default waiting time
+s.download_file('','informe',30) # Here all files that contains string 'informe' in their names will be downloaded with only 30 seconds of waiting to download
+s.download_file('','.xlsx') # Here all files with extension '.xlsx' will be downloaded with default waiting time
+
+### NOTE: CAN'T use REGEX, so a suggestion is first create a list with files you want to download like in the next lines:
+
+## Now let's download only 2 files, you only must set URL where files are located, then the files to download (in list, with their extensions), and optionally the waiting time
+s.download_file('',['informe limpio del 2021-02-02.xlsx','informe limpio del 2021-02-03.xlsx']) # Like before, optionally you can change waiting time after set the list
+
+## For download an entire folder (more robust if you want all files)
+s.download_folder('',"Quantico-informes_diarios") # Here given the url, will download the folder with the name specified, and you can change waiting time
+
+## In case of uploading you must specify the complete route of the file to upload like this:
+archivo = r"C:\Users\IDEAPAD530S\data_todo.dta"
+s.upload_file('',archivo,50) # Finally, specify the url where file will be upload, as before optionally you can change waiting time
+
+## When you finish everything, log out! (security reasons only)
+s.logging_out()
+
+### NOTE: you can make any actions as you want between loggin_in and logging_out!
