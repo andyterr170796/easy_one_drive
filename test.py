fa0f15fdb65bfd53d2685bef98a5fee1e3ae7fb5
andyterr170796 <70968080+andyterr170796@users.noreply.github.com>
2021-03-03 17:10:00 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import os
 os.chdir(r'your_directory') # where the dependencies will be stored
 !git clone https://github.com/andyterr170796/easy_one_drive.git
 
-# Just one time, then only proceed from line 10
+# Just one time (in local, in colab or similars you should put this always), then only proceed from line 10
 import sys
 sys.path.insert(0,os.path.join(os.getcwd(),'easy_one_drive'))
 # Ready to go!
